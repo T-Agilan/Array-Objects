30a0ab0a6858ff4854293b91e4d66221aa5cfb50
T-Agilan <agilanakil1432@gmail.com>
2023-07-31 10:58:39 +0530

    finding average

---
+++ index3.js
@@ -0,0 +1,13 @@
+const numbers = [12, 34, 46, 22, 67, 89];
+let sum=0;
+let length=numbers.length;
+let average;
+function myFunction(){
+for(let i=0;i<numbers.length;i++){
+sum=sum+numbers[i];
+average=sum/length;
+}
+}
+myFunction();
+console.log("The sum of all given numbers:  "+sum);
+console.log("Average:"+average);
