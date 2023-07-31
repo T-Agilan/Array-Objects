9d137309246be0935500de5e36e7752c224a5d09
T-Agilan <agilanakil1432@gmail.com>
2023-07-31 10:26:54 +0530

    finding occurences

---
+++ occurence.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 5, 8, 9, 5, 6, 7, 1, 3, 5];
+let a=0;
+function myFunction(){
+for(let i=0;i<numbers.length;i++){
+    if(numbers[i]===5)
+    a++;
+}
+// dkjfkjhdbakd
+}
+myFunction();
+console.log("The element 5 occurred: "+a);
